b3c6c376c9e4f2709c54d1032cf1b8301d77d971
Erin Gurley <erinmgurley@gmail.com>
2019-07-17 14:33:22 -0700

    post actors test pass

--- test/actors.test.js
+++ test/actors.test.js
@@ -1,20 +1,37 @@
-// require('dotenv').config();
+require('dotenv').config();
 
-// const request = require('supertest');
-// const app = require('../lib/app');
-// const connect = require('../lib/utils/connect');
-// const mongoose = require('mongoose');
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const Actor = require('../lib/models/Actor');
 
-// describe('app routes', () => {
-//   beforeAll(() => {
-//     connect();
-//   });
+describe('actors routes', () => {
+  beforeAll(() => {
+    connect();
+  });
 
-//   beforeEach(() => {
-//     return mongoose.connection.dropDatabase();
-//   });
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
 
-//   afterAll(() => {
-//     return mongoose.connection.close();
-//   });
-// });
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('can POST an actor', () => {
+    const dob = new Date();
+    return request(app)
+      .post('/api/v1/actors')
+      .send({ name: 'Erin', dob, pob: 'Birmingham, AL' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String), 
+          name: 'Erin', 
+          dob: dob.toISOString(),
+          pob: 'Birmingham, AL', 
+          __v: 0
+        });
+      });
+  });
+});
