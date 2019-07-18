aa0cef627619c53d97c4ef62571b93810455467d
Erin Gurley <erinmgurley@gmail.com>
2019-07-18 11:56:08 -0700

    reviewers post test pass

---
+++ test/reviewers.test.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const Reviewer = require('../lib/models/Reviewer');
+
+describe('app routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+  it('can POST a reviewer', () => {
+    return request(app)
+      .post('/api/v1/reviewers')
+      .send({ name: 'Eli', company: 'Alchemy' })
+      .then(res => () => {
+        expect(res.body).toEqual({
+          _id: expect.any(String), 
+          name: 'Eli', company: 'Alchemy', 
+          __v: 0
+        });
+      });
+  }); 
+});
+
