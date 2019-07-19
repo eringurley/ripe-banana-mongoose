d910209bf2f629ba0e62e259d31fd897737da54c
Erin Gurley <erinmgurley@gmail.com>
2019-07-19 16:59:40 -0700

    updated actors test to get only id and name

--- test/actors.test.js
+++ test/actors.test.js
@@ -61,8 +61,10 @@ describe('actors routes', () => {
       .get('/api/v1/actors')
       .then(res => {
         const actorsJSON = JSON.parse(JSON.stringify(actors));
-        actorsJSON.forEach(actor => {
-          expect(res.body).toContainEqual(actor);
+        actorsJSON.forEach(() => {
+          expect(res.body).toContainEqual(
+            { _id: expect.any(String), name: 'Erin' },
+          );
         });
       });
   });
